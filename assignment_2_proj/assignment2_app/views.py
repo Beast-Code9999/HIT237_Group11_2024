9f84b5b158182f66c360f2b9154845cf858e5bfa
Beast Code <beastcode9999@gmail.com>
2024-04-14 15:05:35 +0930

    add base.html and extends to other files

--- assignment_2_proj/assignment2_app/views.py
+++ assignment_2_proj/assignment2_app/views.py
@@ -1,15 +1,15 @@
 from django.shortcuts import render
+from assignment2_app import classes 
 
-
-# Create your views here.
+# http://127.0.0.1:8000/
 def home(request):
     return render(request, 'assignment2_app/index.html')
 
-
+# http://127.0.0.1:8000/project-details
 def projectDetails(request):
 
     context = {
-        
+
     }
 
     return render(request, "assignment2_app/projectDetails.html", context)
