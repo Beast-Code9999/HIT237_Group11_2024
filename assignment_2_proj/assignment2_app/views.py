a5c67644f8f0bcdb035eb3cea9868190d1945e9b
Beast Code <beastcode9999@gmail.com>
2024-04-14 14:06:49 +0930

    add projectDetails() in views.py add projectDetails html add header and footer tags in index.html

--- assignment_2_proj/assignment2_app/views.py
+++ assignment_2_proj/assignment2_app/views.py
@@ -4,3 +4,12 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'assignment2_app/index.html')
+
+
+def projectDetails(request):
+
+    context = {
+        
+    }
+
+    return render(request, "assignment2_app/projectDetails.html", context)
