9406b97a92351f7fc0f63781d8b29b9b7b66a438
Beast Code <beastcode9999@gmail.com>
2024-04-14 16:29:16 +0930

    configure about.html to views.py && urls.py configure base.html to views.py && urls.py configure projectList.html to views.py && urls.py

--- assignment_2_proj/assignment2_app/views.py
+++ assignment_2_proj/assignment2_app/views.py
@@ -6,10 +6,27 @@ def home(request):
     return render(request, 'assignment2_app/index.html')
 
 # http://127.0.0.1:8000/project-details
-def projectDetails(request):
+def project_details(request):
 
     context = {
 
     }
 
     return render(request, "assignment2_app/projectDetails.html", context)
+
+
+def project_list(request):
+
+    context = {
+
+    }
+
+    return render(request, "assignment2_app/projectList.html", context)
+
+def about(request):
+
+    context = {
+
+    }
+
+    return render(request, "assignment2_app/about.html", context)
--- assignment_2_proj/assignment_2_proj/urls.py
+++ assignment_2_proj/assignment_2_proj/urls.py
@@ -21,5 +21,7 @@ from assignment2_app import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     re_path(r'^/?$', views.home, name="home"), # r = raw string, ^ = localhost or http://127.0.0.1:8000/, ? = previous character is optional, $ = ending
-    re_path(r'^project-details/?$', views.projectDetails, name="project-details") # http://127.0.0.1:8000/project-details
+    re_path(r'^project-details/?$', views.project_details, name="project-details"), # http://127.0.0.1:8000/project-details
+    re_path(r'^project-list/?$', views.project_list, name="project-list"), # http://127.0.0.1:8000/project-details
+    re_path(r'^about/?$', views.about, name="about"), # http://127.0.0.1:8000/project-details
 ]
