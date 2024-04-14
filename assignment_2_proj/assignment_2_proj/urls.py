262007066b343a93f8b32b8ef994d4be6c2869ac
Beast Code <beastcode9999@gmail.com>
2024-04-14 14:19:46 +0930

    add settings.py

--- assignment_2_proj/assignment_2_proj/urls.py
+++ assignment_2_proj/assignment_2_proj/urls.py
@@ -20,5 +20,6 @@ from assignment2_app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    re_path(r'^/?$', views.home), # r = raw string, ^ = localhost or http://127.0.0.1:8000/, ? = previous character is optional, $ = ending
+    re_path(r'^/?$', views.home, name="home"), # r = raw string, ^ = localhost or http://127.0.0.1:8000/, ? = previous character is optional, $ = ending
+    re_path(r'^project-details/?$', views.projectDetails, name="project-details")
 ]
