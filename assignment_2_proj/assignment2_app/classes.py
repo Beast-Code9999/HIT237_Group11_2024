932ada30c403b491f19e9f8a4da8169b250a8d1e
Beast Code <beastcode9999@gmail.com>
2024-04-14 15:24:09 +0930

    add ProjectsClass

--- assignment_2_proj/assignment2_app/classes.py
+++ assignment_2_proj/assignment2_app/classes.py
@@ -1,1 +1,20 @@
 # Where we shall write our python classes for Project Details
+
+class ProjectsClass:
+    def __init__(self, supervisor, title, category, topic_num):
+        self.supervisor = supervisor
+        self.title = title
+        self.category = category
+        self.topic_num = topic_num
+
+
+# proj1 = ProjectsClass()        
+# proj2 = ProjectsClass()        
+# proj3 = ProjectsClass()        
+# proj4 = ProjectsClass()        
+# proj5 = ProjectsClass()        
+# proj6 = ProjectsClass()        
+# proj7 = ProjectsClass()        
+
+
+projects_list = []
