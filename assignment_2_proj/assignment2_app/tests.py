d5909b49f0c3bec9a80b71fa66de6e27caac43d3
Beast Code <beastcode9999@gmail.com>
2024-04-17 13:48:53 +0930

    add testings

--- assignment_2_proj/assignment2_app/tests.py
+++ assignment_2_proj/assignment2_app/tests.py
@@ -12,14 +12,11 @@ class HomepageTests(SimpleTestCase):
     def test_template_name_correct(self): # new
         response = self.client.get(reverse("home"))
         self.assertTemplateUsed(response, "index.html")
-    def test_template_content(self): # new
-        response = self.client.get(reverse("index"))
-        self.assertContains(response, "<h1>Homepage</h1>")
 
 
 class AboutpageTests(SimpleTestCase):
     def test_url_exists_at_correct_location(self):
-        response = self.client.get("/about/")
+        response = self.client.get("/about-us/")
         self.assertEqual(response.status_code, 200)
     def test_url_available_by_name(self):
         response = self.client.get(reverse("about"))
@@ -27,6 +24,16 @@ class AboutpageTests(SimpleTestCase):
     def test_template_name_correct(self): # new
         response = self.client.get(reverse("about"))
         self.assertTemplateUsed(response, "about.html")
-    def test_template_content(self): # new  
-        response = self.client.get(reverse("about"))
-        self.assertContains(response, "<h1>About page</h1>")
+
+
+class ProjectlistTests(SimpleTestCase):
+    def test_url_exists_at_correct_location(self):
+        response = self.client.get("/project-list/")
+        self.assertEqual(response.status_code, 200)
+    def test_url_available_by_name(self):
+        response = self.client.get(reverse("project-list"))
+        self.assertEqual(response.status_code, 200)
+    def test_template_name_correct(self): # new
+        response = self.client.get(reverse("project-list"))
+        self.assertTemplateUsed(response, "projectList.html")
+
