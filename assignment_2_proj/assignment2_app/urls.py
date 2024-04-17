2696fa85f0b8404f8565cd7a3b602181d28c87d7
Beast Code <beastcode9999@gmail.com>
2024-04-17 13:30:32 +0930

    changed views.py

--- assignment_2_proj/assignment2_app/urls.py
+++ assignment_2_proj/assignment2_app/urls.py
@@ -3,11 +3,11 @@ from . import views
 
 urlpatterns = [
     # localhost or http://127.0.0.1:8000/
-    path("", views.home, name="home"), 
+    path("/", views.home, name="home"), 
     # http://127.0.0.1:8000/project-list
-    path("project-list", views.project_list, name="project-list"),
+    path("project-list/", views.project_list, name="project-list"),
     # http://127.0.0.1:8000/project-details/<identifier>
-    path("project-details/<slug:slug>", views.project_details, name="project-details"), # project-details/proj1 etc... using path transformer other e.g. is int or str
+    path("project-details/<slug:slug>/", views.project_details, name="project-details"), # project-details/proj1 etc... using path transformer other e.g. is int or str
     # http://127.0.0.1:8000/about
-    path("about-us", views.about, name="about"),   
+    path("about-us/", views.about, name="about"),   
 ]
